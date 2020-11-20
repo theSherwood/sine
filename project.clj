2bf02cc10d752fbe84a58d1caea1e02f0868c228
Adam Sherwood <adam.the.sherwood@gmail.com>
2020-11-19 23:18:23 -0700

    Attempt to rewrite observable in clojurescript.

--- project.clj
+++ project.clj
@@ -1,8 +1,11 @@
 (defproject sine "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  ;; :scm {:name "git" :url "https://github.com/theSherwood/sine"}
+  :license {:name "MIT"
+            :url "https://mit-license.org/"}
+  ;; :deploy-repositories [["releases" :clojars]
+  ;;                       ["snapshots" :clojars]]
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [org.clojure/clojurescript "1.9.521"]]
   :repl-options {:init-ns sine.core}
@@ -25,3 +28,17 @@
                                                :module-type :es6}]
                                :optimizations :simple
                                :pretty-print true}}]})
+
+;; {:main {:source-paths ["src"]
+;;                       :compiler {:main sine.core
+;;                                  :output-to "resources/public/js/compiled/lib.js"
+;;                                  :language-in :ecmascript6
+;;                                  :language-out :ecmascript5
+;;                                  :foreign-libs [{:file "src/sine/observable.js"
+;;                                                  :provides ["observable"]
+;;                                                  :module-type :es6}
+;;                                                 {:file "src/sine/utils.js"
+;;                                                  :provides ["utils"]
+;;                                                  :module-type :es6}]
+;;                                  :optimizations :simple
+;;                                  :pretty-print true}}}
